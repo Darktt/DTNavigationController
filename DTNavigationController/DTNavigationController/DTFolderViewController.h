a5642af21eae9f1672380c399b965ae9390746de
darktt <pippipig@gmail.com>
2013-04-30 15:50:44 +0800

    Create branch

---
+++ DTNavigationController/DTNavigationController/DTFolderViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTFolderViewController.h
+//  DTNavigationController
+//
+//  Created by Darktt on 13/4/30.
+//  Copyright (c) 2013 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTFolderViewController : UIViewController
+
+@end
---
+++ DTNavigationController/DTNavigationController/DTFolderViewController.m
@@ -0,0 +1,38 @@
+//
+//  DTFolderViewController.m
+//  DTNavigationController
+//
+//  Created by Darktt on 13/4/30.
+//  Copyright (c) 2013 Darktt. All rights reserved.
+//
+
+#import "DTFolderViewController.h"
+
+@interface DTFolderViewController ()
+
+@end
+
+@implementation DTFolderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
